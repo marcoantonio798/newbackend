93cbcae17e52c93b73ad742474ad9a2cdc53f653
Marco Antonio <marco.antonio798@gmail.com>
2024-07-18 23:04:31 -0300

    back

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session  
 import os  
-from flask_sqlalchemy import SQLAlchemy  
 
 app = Flask(__name__)  
 app.secret_key = 'sua_chave_secreta'  # Defina uma chave secreta aleatória  
 
-# Configuração do PostgreSQL  
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/dbname'  
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
-db = SQLAlchemy(app)  
-
-# Modelo de Imagem  
-class Image(db.Model):  
-    id = db.Column(db.Integer, primary_key=True)  
-    filename = db.Column(db.String(120), nullable=False)  
+# Dicionário simulando um banco de dados de usuários  
+users = {  
+    'fulano': 'senha123'  
+}  
 
 # Configuração para upload de arquivos  
 UPLOAD_FOLDER = 'static/images'  
@@ -25,35 +19,46 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS  
 
-@app.route('/base')  
-def base():  
-    return render_template('base.html')  
+@app.route('/base')
+def base():
+    return render_template('base.html')
 
 @app.route('/upload', methods=['GET', 'POST'])  
 def upload():  
-    if request.method == 'POST':  
-        if 'file' not in request.files:  
-            return redirect(request.url)  
-        file = request.files['file']  
-        if file.filename == '':  
-            return redirect(request.url)  
-        if file and allowed_file(file.filename):  
-            filename = file.filename  
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))  
-            
-            # Salva a imagem no banco de dados  
-            new_image = Image(filename=filename)  
-            db.session.add(new_image)  
-            db.session.commit()  
-            return redirect(url_for('galeria', new_image=filename))  
-    return render_template('upload.html')  
+    if 'username' in session and session['username'] == 'fulano':  
+        if request.method == 'POST':  
+            if 'file' not in request.files:  
+                return redirect(request.url)  
+            file = request.files['file']  
+            if file.filename == '':  
+                return redirect(request.url)  
+            if file and allowed_file(file.filename):  
+                filename = file.filename  
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))  
+                 
+        return render_template('upload.html')  
+    else:  
+        return redirect(url_for('login'))
 
 @app.route('/')  
 def galeria():  
-    # Recupera as imagens do banco de dados  
-    images = Image.query.all()  
+    images = os.listdir(app.config['UPLOAD_FOLDER'])  # Pega as 3 primeiras imagens  
     new_image = request.args.get('new_image')  
     return render_template('galeria.html', images=images, new_image=new_image)  
+
+
+
+@app.route('/login', methods=['GET', 'POST'])  
+def login():  
+    if request.method == 'POST':  
+        username = request.form['username']  
+        password = request.form['password']  
+        if username in users and users[username] == password:  
+            session['username'] = username  
+            return redirect(url_for('user', username=username))  
+        else:  
+            return render_template('login.html', error='Usuário ou senha inválidos')  
+    return render_template('login.html')  
 
 @app.route('/user/<username>')  
 def user(username):  
@@ -73,6 +78,4 @@ def about():
     return render_template('about.html')
  
 if __name__ == '__main__':  
-    with app.app_context():  
-        db.create_all()  
-    app.run(debug=True)  
+    app.run(debug=True)
